23b3373544109302664a0ae20d852430a7b1f7a0
Eugenio <eugenio.gordillo.arguello@gmail.com>
2024-01-25 23:44:56 -0600

    agregando archivos al repositorio de verdad del proyecto

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { Signale } from "signale";
+
+import { userRouter } from "./user/infrastructure/UserRouter";
+
+
+const app = express();
+
+const signale = new Signale();
+
+app.use(express.json());
+app.use("/users", userRouter);
+
+
+app.listen(3000, () => {
+  signale.success("Server online in port 3000");
+});
